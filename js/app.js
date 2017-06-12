ce7d3e1999e232ea0f28cdaf2d2abb0613a36b63
Shazil Tovar <shazil@gmail.com>
2017-06-12 11:27:52 -0500

    terminado Json buscar planeta

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var cargarPagina = function(){
   cargarPersonajes();
   $(document).on("click", ".personaje", mostrarDetallePersonaje);
+    //cuando los elementos son creados dinamicamente utilizar el evento on click
 }
 
 var cargarPersonajes = function(){
@@ -25,16 +26,26 @@ var mostrarPersonajes = function(personajes){
     var $li = $("<li/>");
    // $li.text(personaje.name);
     $li.addClass("personaje");
-    $li.attr("data-url",personaje.url);//le da el atributo de data-url a cada personaje
+    $li.attr("data-url",personaje.homeworld);//le da el atributo de data-url a cada personaje
     $li.text(personaje.name + " - " + personaje.height + "cm");   //crea un elemento li para cada nombre de personaje
     $ul.append($li);       //agrega cada personaje a la lista
       //console.log(personaje.name);  //obtenemos solo el nombre de los personajes.
   });
     
 }
+var plantillaPlaneta =  '<h2>Planeta:</h2>' +
+   '<p><strong>Nombre:</strong>__nombre__</p>' +
+   '<p><strong>Clima:</strong>__clima__</p>';
+
 var mostrarDetallePersonaje=function(){
-    //console.log(this);
-    alert("hola");
+    var url=$(this).data("url");
+    console.log(url);
+    var $planetaContenedor = $("#planeta");
+    $.getJSON(url,function(response){
+     $planetaContenedor.html(
+     plantillaPlaneta.replace('__nombre__',response.name).replace('__clima__',response.climate)
+        );
+    });
 };
 $(document).ready(cargarPagina);  //es mejor colocarlo despues de haber declarado la función cargarPagina p/hoisting
 
